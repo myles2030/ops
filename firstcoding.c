bdd54c7ad70bcafaee311a081527de2167c3ecae
myles2030 <dbsduddn2000@naver.com>
2020-03-30 11:17:22 +0900

    first c code in linux

---
+++ firstcoding.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello, this is first code with c int linux \n");
+
+	printf("this is calculation program \n");
+
+	int a,b, c=0;
+
+	scanf("%d %d", &a,&b);
+
+	c = a+b;
+
+	printf("This is the result: %d + %d = %d\n", a,b,c);
+
+	return 0;
+}
