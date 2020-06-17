f8bd11268f6df184e61365c985bf234762a13e06
myles2030 <dbsduddn2000@naver.com>
2020-06-17 14:48:42 +0900

    adding txt file

--- forproject/app.py
+++ forproject/app.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
 
 from flask import Flask, render_template,request
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
 
 @app.route('/')
 def send():
     return render_template('web.html', address=0)
 
-@app.route('/urladdress', methods=['GET'])
+@app.route('/urladdress', methods=['GET', 'POST'])
 def urladdress():
-    if request.method == 'GET':
+    if request.method == 'POST':
         temp = request.args.get('address')
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
 
-    return render_template('web.html', address=temp)
+    return render_template('web.html', address= 'file uploaded successfully')
+
+if __name__ == "__main__":
+    app.run()
+
 
 
 
