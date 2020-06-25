79913b8c1441b5f0c90758b9429cce6568798c36
wand0812 <dldhks2@gmail.com>
2020-06-26 07:36:37 +0900

    new app.py

--- forproject/app.py
+++ forproject/app.py
@@ -2,21 +2,33 @@
 
 from flask import Flask, render_template,request
 from werkzeug.utils import secure_filename
+from urllib.request import urlopen 
+from bs4 import BeautifulSoup
+import re
 
 app = Flask(__name__)
 
 @app.route('/')
 def send():
-    return render_template('web.html', address=0)
+    return render_template('web.html', results=0)
 
 @app.route('/urladdress', methods=['GET', 'POST'])
 def urladdress():
-    if request.method == 'POST':
-        temp = request.args.get('address')
-        f = request.files['file']
-        f.save(secure_filename(f.filename))
+        temp = request.args.get('results')
+        #f = request.files['file']
+        #f.save(secure_filename(f.filename))
+        
+        webpage = urlopen(temp)
+        bs = BeautifulSoup(webpage,'html.parser')
+        tags = str(bs.findAll("div",{"class":"section"}))
 
-    return render_template('web.html', address= 'file uploaded successfully')
+        tags = re.sub('<.+?>',"",tags,0).strip()
+
+        b = tags.replace('(',' ').replace(')',' ').replace(',',' ').replace('.',' ')replace(';',' ').replace('"',' ').replace("'"," ").split()
+
+        count = len(b)
+        
+        return render_template('web.html', results = count)
 
 if __name__ == "__main__":
     app.run()
