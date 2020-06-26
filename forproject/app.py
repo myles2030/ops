1317d9dcf694f0417fc9ee05e875be2da3d2dada
myles2030 <dbsduddn2000@naver.com>
2020-06-26 22:52:32 +0900

    adding texting

--- forproject/app.py
+++ forproject/app.py
@@ -2,25 +2,77 @@
 
 from flask import Flask, render_template,request
 from werkzeug.utils import secure_filename
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+import time
 
 app = Flask(__name__)
 
 @app.route('/')
 def send():
-    return render_template('web.html', address=0)
+    return render_template('web.html', results=0)
 
 @app.route('/urladdress', methods=['GET', 'POST'])
 def urladdress():
-    if request.method == 'POST':
-        temp = request.args.get('address')
-        f = request.files['file']
-        f.save(secure_filename(f.filename))
+    temp = request.args.get('results')
+    
+    webpage = urlopen(temp)
+    bs = BeautifulSoup(webpage,'html.parser')
+    tags = str(bs.findAll("div",{"class":"section"}))
 
-    return render_template('web.html', address= 'file uploaded successfully')
+    tags = re.sub('<.+?>',"",tags,0).strip()
+
+    start = time.time()
+
+    b = tags.replace('(',' ').replace(')',' ').replace(',',' ').replace('.',' ').replace(';',' ').replace('"',' ').replace("'"," ").split()
+        
+    count = len(b)
+
+    endtime = time.time()-start
+
+    return render_template('web.html', results = count)
+
+@app.route('/textfile', methods = ['GET','POST'])
+def textfile():
+    f = request.files['file']
+    f.save(secure_filename(f.filename))
+    
+    tf = open("text.txt", "r")
+
+    urldata = list()
+    urlvalue = list()
+    countingline = 0
+
+    while True:
+        line = tf.readline()
+        if not line: break
+        urldata.append(line[:-1])
+        countingline = countingline + 1
+
+    datacount = list()
+
+    for urllist in urldata:
+        twebpage = urlopen(urllist)
+        ash = BeautifulSoup(twebpage,'html.parser')
+        buck = str(ash.findAll("div",{"class":"section"}))
+
+        urlvalue.append("success")
+
+        #fail: no url, and url that can't crawling.
+
+        buck = re.sub('<.+?>',"",buck,0).strip()
+
+        glaz = buck.replace('(',' ').replace(')',' ').replace(',',' ').replace('.',' ').replace(";"," ").replace('"',' ').replace("'"," ").split()
+    
+        datacount.append(len(glaz))
+
+    return render_template('web.html',results = datacount)
+
+
 
 if __name__ == "__main__":
     app.run()
-
 
 
 
